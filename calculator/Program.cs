b13b8dbe2f0670a100d8d4acb990c9f051ec66af
Marwan Abdelhamied <marawanelsawy00@gmail.com>
2026-09-10 22:25:10 +0300

    feat: basic calculator initial code

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿string operation;
+double firstNumber, secondNumber, result;
+Console.WriteLine(" Welcome To My Humble Calculator . . . ");
+
+//reading first number
+Console.WriteLine("Enter First valid number");
+firstNumber = double.Parse(Console.ReadLine());
+
+
+//reading operation type 
+Console.WriteLine("Choose Operator + - * / %");
+operation = Console.ReadLine();
+
+//reading second number
+Console.WriteLine("Enter second valid number");
+secondNumber = double.Parse(Console.ReadLine());
+
+
+switch (operation)
+{
+    case "+": result = firstNumber + secondNumber; break;
+    case "-": result = firstNumber - secondNumber; break;
+    case "*": result = firstNumber * secondNumber; break;
+    case "/": result = firstNumber / secondNumber; break;
+    case "%": result = firstNumber % secondNumber; break;
+    default: result = 0; break;
+}
+
+
+Console.WriteLine($"the result is => {result}");
